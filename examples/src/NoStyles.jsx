0cbf5573c6677ad61b59db026040ca6b2b3b0833
Ariel Rodriguez <ariel.fdr@gmail.com>
2017-09-02 21:32:44 -0300

    [refactor] Better NoStyle example.

--- examples/src/NoStyles.jsx
+++ examples/src/NoStyles.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import Access from './shared/Access'
-import { renderToString } from '../../lib'
+import RAMPT from '../../lib'
+
+const AMPTemplate = new RAMPT({
+  title: 'Creating AMP Page with React',
+  canonical: 'https://my-website.com',
+})
+
+const SocialShare = (props, { template }) => {
+  template.register('amp-social-share')
+  return <amp-social-share {...props} />
+}
+
+
+const Body = (props, context) => {
+  // Add required meta tags to head at any moment.
+  context.template.register('meta', {'http-equiv': "origin-trial", 'data-feature': "Web Share"})
+
+  return(
+    <body>
+      <h1>Preact for AMP is welcome as well!</h1>
+      <SocialShare width="40" height="40" type="email"/>
+      <SocialShare width="40" height="40" type="facebook"
+        data-param-app_id="254325784911610" />
+      <SocialShare width="40" height="40" type="gplus"/>
+      <SocialShare width="40" height="40" type="linkedin"/>
+      <SocialShare width="40" height="40" type="whatsapp"/>
+      <SocialShare width="40" height="40" type="twitter"/>
+    </body>
+  )
+}
 
 export default class Aphrodite {
   static render() {
-    return renderToString(<body><Access /></body>,
-      { title: 'Awesome!', canonical: 'https://canonical.com' })
+    return AMPTemplate.renderToString(<Body />)
   }
 }

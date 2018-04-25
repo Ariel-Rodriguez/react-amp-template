4b5b0c24ca84d1a022286e3e25c7ff556a9e66ba
Ariel Rodriguez <ariel.fdr@gmail.com>
2018-04-25 03:18:59 -0300

    add: AMP component import validation.

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,34 +6,33 @@ import Tag from './Tag'
 import Title from './Title'
 
 /**
- * Transforms to toKebabCase for each upper case coincidence.
- * @returns {String} e.g -sticky-ad
+ * Transforms input to hyphened lowercase for each upper case coincidence.
  * @param {String} str React custom-element name e.g StickyAd
+ * @returns {String} the input as kebabCase with leaded by a hyphen e.g. sticky-ad
  */
 const toKebabCase = str =>
-  str.replace(/([A-Z])/g, '-$1').toLowerCase()
+  str.replace(/([A-Z])/g, '-$1').slice(1).toLowerCase()
 
-// Primitives RAMPT components to be exposed.
-const PublicComponents = {
-  Link, Meta, Script, Tag, Title,
+function derivatedScriptComponent(name) {
+  if (!/^[A-Z]/.test(name)) {
+    throw Error(`Failed to fallback component name ${name}. Component to import must begin with uppercase.`)
+  }
+  return function scriptComponent(props) {
+    return React.createElement(Script, {
+      _name: `amp-${toKebabCase(name)}`,
+      ...props,
+    })
+  }
 }
 
 /**
- * @description Exposes a factory to generate components in runtime
+ * @description Exposes a factory to generate AMP components in runtime.
  * @returns {Object} React component
  */
-export default new Proxy(PublicComponents, {
-  get(object, property) {
-    if (property in object) {
-      return object[property]
-    }
-
-    // defaults to amp-element
-    return props => (
-      React.createElement(Script, {
-        _name: `amp${toKebabCase(property)}`,
-        ...props,
-      })
-    )
+export default new Proxy({
+  Link, Meta, Script, Tag, Title, toKebabCase,
+}, {
+  get(thisModule, componentName) {
+    return thisModule[componentName] || derivatedScriptComponent(componentName)
   },
 })

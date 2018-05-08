ba4f57381db195f4c58d4ba61e1b108e3708a6aa
Ariel Rodriguez <ariel.fdr@gmail.com>
2018-05-08 08:55:46 -0300

    fix: simple-server example to start.

--- examples/simple-server/server/render.js
+++ examples/simple-server/server/render.js
@@ -2,18 +2,33 @@ const fs = require('fs')
 const { resolve } = require('path')
 const app = require('../dist/App').default
 const json = require('./data.json')
+const mkdirp = require('mkdirp')
 
-const html = app({
-  title: 'test',
-  date: Date().substring(0, 15),
-  json,
-})
+const dest = resolve('./public')
 
-const dest = resolve('./public/index.html')
+function onError(err) {
+  throw err
+}
 
-fs.writeFile(dest, html, err => {
-  if (err) {
-    throw Error(err)
-  }
-  console.log(`\n ***  AMP generated in ${dest}\n`)
-})
+function renderApp(cb) {
+  const html = app({
+    title: 'test',
+    date: Date().substring(0, 15),
+    json,
+  })
+
+  fs.writeFile(`${dest}/index.html`, html, err => {
+    if (err) return onError(err)
+
+    console.log(`\n ***  AMP generated in ${dest}\n`)
+  })
+}
+
+function createDir(cb) {
+  mkdirp(dest, err => {
+    if (err) return onError(err)
+    return cb()
+  })
+}
+
+createDir(renderApp)

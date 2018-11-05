af6ee3a478af69a7b907ae3a636c4a0e71e46013
Ariel Rodriguez <ariel.fdr@gmail.com>
2018-11-05 19:32:27 +0100

    Update unit test (an integration test almost)

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import test from 'ava'
-import { renderToString } from '../src'
+import { renderToString, AMP } from '../src'
 import styled from 'styled-components'
 
-test('RAMPT render', async t => {
+test('it renders simple node element', async t => {
   const body = React.createElement('body', {})
   const output = renderToString(body)
   t.regex(output, /<body><\/body>/, 'Renders HTML template with body element.')
 })
+
+const renderAllAMPComponents = () => {
+  const Title = React.createElement(AMP.Title, {}, 'title')
+  const Link = React.createElement(AMP.Link, { src: 'https://link' }, 'link')
+  const body = React.createElement('body', {}, [Title, Link])
+  return renderToString(body)
+}
+
+test('it renders all AMP node element', t => {
+  const render1 = renderAllAMPComponents()
+  const render2 = renderAllAMPComponents()
+  t.true(render1 === render2, 'Each render should not mix the state between each other.')
+  t.snapshot(render1)
+})
+
 
 test('RAMPT render with styles', async t => {
   const styledBody = styled.body`background: red;`
